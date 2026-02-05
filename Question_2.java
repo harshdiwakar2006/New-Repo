b8ea7b791c96f8ff96e6afdc0d6aabe7baf2b3ed
Harsh Diwakar <harshdiwakar2006@gmail.com>
2026-02-05 20:04:39 +0530

    first commit

---
+++ Question_2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Question_2 {
+     public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        // Taking input
+        System.out.println("Enter No. of elements : ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        // fatching output
+        System.out.print("Elements of array: ");
+        for (int i=0; i<n;i++){
+            System.out.print(arr[i]*arr[i]+" ");
+        }
+
+     }
+}
